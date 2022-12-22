3ad91bfad349b86ac5089789c623d8b91f5e1573
danarbastian7 <danarbastian7@gmail.com>
2022-12-22 10:48:57 +0900

    first commit

---
+++ projects/client/src/App.jsx
@@ -0,0 +1,69 @@
+import axios from "axios"
+
+import { useEffect, useState } from "react"
+
+import { Route, Router, Routes, useLocation } from "react-router-dom"
+
+import { Link } from "react-router-dom"
+
+import { useDispatch, useSelector } from "react-redux"
+import { axiosInstance } from "./api/index"
+import { login } from "./redux/features/authSlice"
+
+import ListingDetails from "./pages/listing/ListingDetails"
+
+function App() {
+  const authSelector = useSelector((state) => state.auth)
+  console.log(authSelector, "test")
+  const [message, setMessage] = useState("")
+  const location = useLocation()
+
+  const [authCheck, setAuthCheck] = useState(false)
+  const dispatch = useDispatch()
+  const keepUserLoggedIn = async () => {
+    try {
+      const auth_token = localStorage.getItem("auth_token")
+
+      if (!auth_token) {
+        setAuthCheck(true)
+        return
+      }
+
+      const response = await axiosInstance.get("/auth/refresh-token", {
+        headers: {
+          authorization: `Bearer ${auth_token}`,
+        },
+      })
+      console.log(response)
+
+      dispatch(login(response.data.data))
+      localStorage.setItem("auth_token", response.data.token)
+    } catch (err) {
+      console.log(err)
+      setAuthCheck(true)
+    }
+  }
+  useEffect(() => {
+    keepUserLoggedIn()
+  }, [])
+
+  useEffect(() => {
+    ;(async () => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/greetings`
+      )
+      setMessage(data?.message || "")
+    })()
+  }, [])
+
+  return (
+    <main>
+      <Routes>
+        <Route path="/listing/details/:id" element={<ListingDetails />} />
+      </Routes>
+      {/* <Footer /> */}
+    </main>
+  )
+}
+
+export default App
